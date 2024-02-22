dda9abe236cecff884f7fb08115b3e284a2e2194
TobiaRigon <rigontobia@gmail.com>
2024-02-22 18:43:07 +0100

    Feat: Bootstrap easy layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,16 +3,25 @@
     <title>Home</title>
 @endsection
 @section('content')
-<h1>Projects</h1>
-<ul>
-    @foreach ($types as $type)
-        @foreach ($type->projects as $project)
-            <li>
-                <h3>{{$project->name}}</h3>
-                <p>{{$project->description}}</p>
-                <p><strong>Type:</strong> {{$type->name}}</p>
-            </li>
+<div class="container">
+    <h1 class="mt-5 mb-4">Projects</h1>
+    <div class="row my-3">
+        @foreach ($types as $type)
+            @foreach ($type->projects as $project)
+                <div class="col-md-4">
+                    <div class="card project-card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$project->name}}</h5>
+                            <p class="card-text">{{$project->description}}</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted"><strong>Type:</strong> {{$type->name}}</small>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         @endforeach
-    @endforeach
-</ul>
+    </div>
+</div>
+
 @endsection
