15aa7f880d305a7b496cd017c1f5074cb9c1bec9
TobiaRigon <rigontobia@gmail.com>
2024-02-22 17:46:03 +0100

    Create: seeders + seeders setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this -> call([
-       TypeTableSeeder :: class
+       TypeTableSeeder :: class,
+       ProjectTableSeeder :: class
        ]);
     }
 }
--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Project;
+use App\Models\Type;
+
+
 class ProjectTableSeeder extends Seeder
 {
     /**
@@ -14,6 +18,16 @@ class ProjectTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Project :: factory()
+        ->count(100)
+        ->make()
+        ->each(function($project){
+
+            $type = Type :: inRandomOrder() -> first();
+            $project -> type() -> associate($type);
+
+            $project -> save();
+
+        });
     }
 }
