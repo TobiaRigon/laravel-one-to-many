650487a7f9ddb971bd7e6a6d29760a3bd0357073
TobiaRigon <rigontobia@gmail.com>
2024-02-22 18:34:02 +0100

    Feat: index data print

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,16 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+<h1>Projects</h1>
+<ul>
+    @foreach ($types as $type)
+        @foreach ($type->projects as $project)
+            <li>
+                <h3>{{$project->name}}</h3>
+                <p>{{$project->description}}</p>
+                <p><strong>Type:</strong> {{$type->name}}</p>
+            </li>
+        @endforeach
+    @endforeach
+</ul>
 @endsection
